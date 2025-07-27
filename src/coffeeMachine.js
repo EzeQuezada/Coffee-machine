cbdf9685777f1abf433e8697573639584238f266
EzeQuezada <20231021.itla.edu.do>
2025-07-27 16:38:11 -0400

    Subida inicial de archivos del proyecto TDD

---
+++ src/coffeeMachine.js
@@ -0,0 +1,32 @@
+class CoffeeMachine {
+    constructor() {
+        this.cupSize = null;
+        this.sugarSpoons = 0;
+    }
+
+    selectCupSize(size) {
+        const validSizes = ['small', 'medium', 'large'];
+        if (validSizes.includes(size)) {
+            this.cupSize = size;
+        } else {
+            throw new Error('Invalid cup size selected. Please choose small, medium, or large.');
+        }
+    }
+
+    addSugar(spoons) {
+        if (typeof spoons === 'number' && spoons >= 0) {
+            this.sugarSpoons = spoons;
+        } else {
+            throw new Error('Invalid number of sugar spoons. Please enter a non-negative number.');
+        }
+    }
+
+    getOrderDetails() {
+        if (!this.cupSize) {
+            throw new Error('Cup size not selected.');
+        }
+        return `You have ordered a ${this.cupSize} coffee with ${this.sugarSpoons} sugar spoon(s).`;
+    }
+}
+
+export default CoffeeMachine;
